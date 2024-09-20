50e129e404e4260120db58630d3412a4d6d319a9
Alisson <llielzinh@gmail.com>
2024-09-20 20:54:02 -0300

    fix array

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const $containerdequestoes = document.querySelector(".container-de-questoes")
 const $containerfase2 = document.querySelector(".container-fase-2")
 const $containerfase3 = document.querySelector(".container-fase-3")
 
+// array de pontuação
 var pontos = []
 
-//esconde as perguntas das fases 2 e 3
+// esconde as perguntas das fases 2 e 3
 $containerfase2.classList.add("hide")
 $containerfase3.classList.add("hide")
 
