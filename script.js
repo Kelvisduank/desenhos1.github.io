1397b1d8279c112c99eb4f429f9648e4bb17962b
Alisson <llielzinh@gmail.com>
2024-09-19 23:03:03 -0300

    build: array de pontos

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const $containerdequestoes = document.querySelector(".container-de-questoes")
 const $containerfase2 = document.querySelector(".container-fase-2")
 const $containerfase3 = document.querySelector(".container-fase-3")
 
+var pontos = []
+
 //esconde as perguntas das fases 2 e 3
 $containerfase2.classList.add("hide")
 $containerfase3.classList.add("hide")
@@ -19,7 +21,9 @@ function answer(n){
     //alert(n) n --> é o value do button clicado.
     if (n == 1){
         alert("correto")
+        pontos.push(10)
     }else{
         alert("errou")
     }
+    alert(`Você fez ${pontos[pontos.length -1]} pontos`)
 }        
