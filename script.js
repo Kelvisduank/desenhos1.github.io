91ce699346e177ab0d573c26a59b6081ca2f14de
Alisson <llielzinh@gmail.com>
2024-09-22 18:01:33 -0300

    build: question 3

--- script.js
+++ script.js
@@ -6,18 +6,35 @@ const $container1 = document.querySelector(".container1") // Corrigido o seletor
 var passou = false
 const $container = document.querySelector(".container")
 const $containerdequestoes1 = document.querySelector(".container-de-questoes1")
+//container da 3a pergunta
+const $container2 = document.querySelector(".container2")
+const $containerdequestoes2 = document.querySelector(".container-de-questoes2")
 
 
 $botaoComecar.addEventListener("click", startGame)
 
+//perguntas 1, 2 e 3
 $container.classList.remove("hide")
 $container1.classList.add("hide")
+$container2.classList.add("hide")
 
 function startGame() {
     $botaoComecar.classList.add("hide")
     $containerdequestoes.classList.remove("hide")
     $container1.classList.add("hide")
     
+}
+
+function nextlevel(passar){    
+       
+    //sumir fase 1
+    $containerdequestoes.classList.add("hide")
+    $container.classList.add("hide")
+
+    //passar fase 2
+    $containerdequestoes1.classList.remove("hide")
+    $container1.classList.remove("hide")
+   
 }
 
 // Verifica resposta
@@ -32,23 +49,19 @@ function answer(n) {
     }
 }
 
-function nextlevel(passar){    
-       
-    //sumir fase 1
-    $containerdequestoes.classList.add("hide")
-    $container.classList.add("hide")
-
-
-        $containerdequestoes1.classList.remove("hide")
-        
-
-        //passar fase 2
-        $container1.classList.remove("hide")
-
-    
-}
-function answer1(n){
+function answer1(n){ //checa a 2a questão
     if (n == 3){
+        alert("Acertou")
+        passou = true
+    }
+    else{
+        alert("errou")
+        passou = false
+    }
+}
+
+function answer2(n){ //checa a 3a questão
+    if (n == 2){
         alert("Acertou")
         passou = true
     }
