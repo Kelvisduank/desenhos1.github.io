23bb8262a3c875b1b2c4bd233b9e3a4167ed626f
Maria Eduarda <125852009+Mariaeduardamatias@users.noreply.github.com>
2024-09-28 00:46:00 -0300

    colocando icones

--- dev.js
+++ dev.js
@@ -4,35 +4,70 @@ const developers = {
         name: "Kelvis Duank",
         description: "Sou Kelvis Duank, um jovem de 16 anos do povo indígena Xakriabá, atualmente cursando o Ensino Médio Integrado com técnico em Informática para Internet no Instituto Federal Goiano - Campus Urutaí, e também faço parte do programa Programadores do Amanhã. Minha paixão pela tecnologia começou ainda na infância, e sigo cultivando esse interesse, sempre buscando aprender novas ferramentas e tendências. Nos meus momentos livres, além de explorar o mundo da tecnologia, gosto de praticar esportes, especialmente vôlei.",
         socials: `<a href="kelvisduankxkb.netlify.app" target="_blank">Portifólio</a>
-                  <a href="https://github.com/Kelvisduank" target="_blank">GitHub</a>`
+                  <a href="https://github.com/Kelvisduank" target="_blank">GitHub
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                        <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.387.6.11.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.387-1.332-1.757-1.332-1.757-1.087-.744.084-.729.084-.729 1.205.085 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.997.108-.775.418-1.305.76-1.605-2.665-.305-5.466-1.333-5.466-5.93 0-1.31.47-2.38 1.235-3.22-.123-.305-.535-1.535.117-3.2 0 0 1.005-.322 3.3 1.23a11.47 11.47 0 013-.405c1.02.005 2.045.137 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.66 1.665.248 2.895.12 3.2.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.43.37.81 1.1.81 2.22 0 1.605-.015 2.9-.015 3.3 0 .32.215.694.825.577C20.565 22.092 24 17.592 24 12.297 24 5.67 18.63.297 12 .297z"/>
+                     </svg>
+                  </a>`
     },
     dev2: {
         name: "Kaue",
         description: "Sou Kauê Santos Calixto, estudante de Tecnologia em Desenvolvimento Back-end na Anhanguera, com experiência prática em suporte técnico e administração de sistemas. Durante minha atuação como Help Desk na Aliançanet Telecom Ltda, desenvolvi habilidades em troubleshooting e manutenção de redes, dispositivos, e sistemas operacionais como Windows 10, Mac OS, e Android. Além disso, possuo conhecimento em serviços de nuvem como AWS e em fundamentos de redes. Estou focado em avançar na carreira como Analista de Suporte, onde posso aplicar minha expertise em diversas plataformas para garantir a eficiência e a estabilidade dos ambientes de TI.",
-        socials: `<a href="https://www.linkedin.com/in/kauescalixto/" target="_blank">LinkedIn</a>
-                  <a href="https://github.com/kauecalixto" target="_blank">GitHub</a>`
+        socials: `<a href="https://www.linkedin.com/in/kauescalixto/" target="_blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                     <path d="M22.23 0H1.77C.79 0 0 .77 0 1.75v20.5C0 23.23.77 24 1.77 24h20.46c.98 0 1.77-.77 1.77-1.75V1.75C24 .77 23.23 0 22.23 0zM7.12 20.45H3.56V9h3.56v11.45zM5.34 7.55c-1.14 0-1.87-.77-1.87-1.75 0-.98.74-1.75 1.89-1.75S7.21 5 7.21 6.03c-.01.98-.74 1.75-1.87 1.75zM20.45 20.45h-3.55v-5.67c0-1.41-.5-2.37-1.75-2.37-.95 0-1.52.64-1.77 1.25-.09.23-.11.55-.11.87v5.93H9.79V9h3.42v1.55h.05c.43-.72 1.22-1.55 2.96-1.55 2.16 0 3.78 1.41 3.78 4.43v6.02z"/>
+                 </svg>
+                  </a>
+                  <a href="https://github.com/kauecalixto" target="_blank">
+                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                        <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.387.6.11.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.387-1.332-1.757-1.332-1.757-1.087-.744.084-.729.084-.729 1.205.085 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.997.108-.775.418-1.305.76-1.605-2.665-.305-5.466-1.333-5.466-5.93 0-1.31.47-2.38 1.235-3.22-.123-.305-.535-1.535.117-3.2 0 0 1.005-.322 3.3 1.23a11.47 11.47 0 013-.405c1.02.005 2.045.137 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.66 1.665.248 2.895.12 3.2.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.43.37.81 1.1.81 2.22 0 1.605-.015 2.9-.015 3.3 0 .32.215.694.825.577C20.565 22.092 24 17.592 24 12.297 24 5.67 18.63.297 12 .297z"/>
+                     </svg>
+                  </a>`
     },
     dev3: {
         name: "Alisson Nunes",
         description: "Oi, meu nome é Alisson, tenho 20 anos, conclui o ensino médio, atualmente sou aluno do PDA e estou ansioso para faculdade e meu 1o emprego como programador, programar para mim é um sonho, graças aos incentivos do meu pai fui me fascinando em tecnologia e descobrindo o mundo da programação e agora aqui estou...",
         socials: ` <a href="https://www.linkedin.com/in/alisson-samuel-994065247/" target="_blank">
-                <i class="fab fa-linkedin"></i> LinkedIn
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                 <path d="M22.23 0H1.77C.79 0 0 .77 0 1.75v20.5C0 23.23.77 24 1.77 24h20.46c.98 0 1.77-.77 1.77-1.75V1.75C24 .77 23.23 0 22.23 0zM7.12 20.45H3.56V9h3.56v11.45zM5.34 7.55c-1.14 0-1.87-.77-1.87-1.75 0-.98.74-1.75 1.89-1.75S7.21 5 7.21 6.03c-.01.98-.74 1.75-1.87 1.75zM20.45 20.45h-3.55v-5.67c0-1.41-.5-2.37-1.75-2.37-.95 0-1.52.64-1.77 1.25-.09.23-.11.55-.11.87v5.93H9.79V9h3.42v1.55h.05c.43-.72 1.22-1.55 2.96-1.55 2.16 0 3.78 1.41 3.78 4.43v6.02z"/>
+                 </svg>
             </a>
             <a href="https://github.com/alissonn17" target="_blank">
-                <i class="fab fa-github"></i> GitHub
+                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                   <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.387.6.11.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.387-1.332-1.757-1.332-1.757-1.087-.744.084-.729.084-.729 1.205.085 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.997.108-.775.418-1.305.76-1.605-2.665-.305-5.466-1.333-5.466-5.93 0-1.31.47-2.38 1.235-3.22-.123-.305-.535-1.535.117-3.2 0 0 1.005-.322 3.3 1.23a11.47 11.47 0 013-.405c1.02.005 2.045.137 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.66 1.665.248 2.895.12 3.2.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.43.37.81 1.1.81 2.22 0 1.605-.015 2.9-.015 3.3 0 .32.215.694.825.577C20.565 22.092 24 17.592 24 12.297 24 5.67 18.63.297 12 .297z"/>
+                 </svg>
             </a>`
     },
     dev4: {
         name: "Maria Eduarda Souza",
         description: "Meu nome é Maria Eduarda Silva Souza, tenho 19 anos e sou de São Paulo. Sou formada em Técnico em Desenvolvimento de Sistemas pela Etec. Possuo habilidades básicas em programação com HTML, CSS, JavaScript, MySQL, Java, além de conhecimento em gerenciamento de projetos. Atualmente, estou fazendo um curso online de programação pelo Programa Programadores do Amanhã e em busca de novas oportunidades que me permitam aplicar e aprimorar minhas habilidades em ambientes desafiadores e inovadores. Fora do trabalho, gosto de viajar com minha família e estudar sobre novas tecnologias.",
-        socials: `<a href="https://www.linkedin.com/in/maria-eduarda-souza-919841276/" target="_blank">LinkedIn</a>
-                  <a href="https://github.com/Mariaeduardamatias" target="_blank">GitHub</a>`
+
+        socials: `<a href="https://github.com/Mariaeduardamatias" target="_blank">
+                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                        <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.387.6.11.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.387-1.332-1.757-1.332-1.757-1.087-.744.084-.729.084-.729 1.205.085 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.997.108-.775.418-1.305.76-1.605-2.665-.305-5.466-1.333-5.466-5.93 0-1.31.47-2.38 1.235-3.22-.123-.305-.535-1.535.117-3.2 0 0 1.005-.322 3.3 1.23a11.47 11.47 0 013-.405c1.02.005 2.045.137 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.66 1.665.248 2.895.12 3.2.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.43.37.81 1.1.81 2.22 0 1.605-.015 2.9-.015 3.3 0 .32.215.694.825.577C20.565 22.092 24 17.592 24 12.297 24 5.67 18.63.297 12 .297z"/>
+                     </svg>
+                </a>
+
+                <a href="https://www.linkedin.com/in/maria-eduarda-souza-919841276/" target="_blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                         <path d="M22.23 0H1.77C.79 0 0 .77 0 1.75v20.5C0 23.23.77 24 1.77 24h20.46c.98 0 1.77-.77 1.77-1.75V1.75C24 .77 23.23 0 22.23 0zM7.12 20.45H3.56V9h3.56v11.45zM5.34 7.55c-1.14 0-1.87-.77-1.87-1.75 0-.98.74-1.75 1.89-1.75S7.21 5 7.21 6.03c-.01.98-.74 1.75-1.87 1.75zM20.45 20.45h-3.55v-5.67c0-1.41-.5-2.37-1.75-2.37-.95 0-1.52.64-1.77 1.25-.09.23-.11.55-.11.87v5.93H9.79V9h3.42v1.55h.05c.43-.72 1.22-1.55 2.96-1.55 2.16 0 3.78 1.41 3.78 4.43v6.02z"/>
+                     </svg>
+                </a>`
+
     },
     dev5: {
         name: "Tayssa Ramos",
         description: "Sou Tayssa Ramos, tenho 16 anos, estou no 2° ano do ensino médio, atualmente sou aluna do PDA e estou ansiosa para concluir meu ensino médio, sempre fui fascinada por tecnologia mas não pensava a seguir carreira , entrei nesse mundo de programação agora , e curiosa sobre o que posso aprender",
-        socials: `<a href="https://www.linkedin.com/in/tayssa-ramos-6523a2318/" target="_blank">LinkedIn</a>
-                  <a href="https://github.com/dev5" target="_blank">GitHub</a>`
+        socials: `<a href="https://www.linkedin.com/in/tayssa-ramos-6523a2318/" target="_blank">
+                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                             <path d="M22.23 0H1.77C.79 0 0 .77 0 1.75v20.5C0 23.23.77 24 1.77 24h20.46c.98 0 1.77-.77 1.77-1.75V1.75C24 .77 23.23 0 22.23 0zM7.12 20.45H3.56V9h3.56v11.45zM5.34 7.55c-1.14 0-1.87-.77-1.87-1.75 0-.98.74-1.75 1.89-1.75S7.21 5 7.21 6.03c-.01.98-.74 1.75-1.87 1.75zM20.45 20.45h-3.55v-5.67c0-1.41-.5-2.37-1.75-2.37-.95 0-1.52.64-1.77 1.25-.09.23-.11.55-.11.87v5.93H9.79V9h3.42v1.55h.05c.43-.72 1.22-1.55 2.96-1.55 2.16 0 3.78 1.41 3.78 4.43v6.02z"/>
+                         </svg>
+                  </a>
+                  <a href="https://github.com/dev5" target="_blank">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                             <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.387.6.11.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.387-1.332-1.757-1.332-1.757-1.087-.744.084-.729.084-.729 1.205.085 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.997.108-.775.418-1.305.76-1.605-2.665-.305-5.466-1.333-5.466-5.93 0-1.31.47-2.38 1.235-3.22-.123-.305-.535-1.535.117-3.2 0 0 1.005-.322 3.3 1.23a11.47 11.47 0 013-.405c1.02.005 2.045.137 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.66 1.665.248 2.895.12 3.2.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.43.37.81 1.1.81 2.22 0 1.605-.015 2.9-.015 3.3 0 .32.215.694.825.577C20.565 22.092 24 17.592 24 12.297 24 5.67 18.63.297 12 .297z"/>
+                     </svg>
+                  </a>`
     },
    
 };
